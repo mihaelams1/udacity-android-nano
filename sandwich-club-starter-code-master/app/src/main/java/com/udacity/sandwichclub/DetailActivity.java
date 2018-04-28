ae74e80530f7eeec6ae317c7004dbe3f34256a89
Mihaela Korte <mihaelams1@gmail.com>
2018-04-28 14:48:48 -0700

    Sandwich project v1

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,8 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
+
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +60,49 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        String unknown = getString(R.string.unknown);;
 
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        String alsoKnownAs = unknown;
+        if (alsoKnownAsList != null && alsoKnownAsList.size() > 0) {
+            alsoKnownAs = convertToString(alsoKnownAsList);
+        }
+        alsoKnownAsTv.setText(alsoKnownAs);
+
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        String description = sandwich.getDescription();
+        if (description == null || description.isEmpty()) {
+            description = unknown;
+        }
+        descriptionTv.setText(description);
+
+        TextView originTv = findViewById(R.id.origin_tv);
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        if (placeOfOrigin == null || placeOfOrigin.isEmpty()) {
+            placeOfOrigin = unknown;
+        }
+        originTv.setText(placeOfOrigin);
+
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        List<String> ingredientsList = sandwich.getIngredients();
+        String ingredients = unknown;
+        if (ingredientsList != null && ingredientsList.size() > 0) {
+            ingredients = convertToString(ingredientsList);
+        }
+        ingredientsTv.setText(ingredients);
+    }
+
+    private static String convertToString(List<String> listOfString) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : listOfString)
+        {
+            sb.append(s).append(", ");
+        }
+
+        String result = sb.toString();
+        result = result.substring(0, result.length() - 2);
+        return result;
     }
 }
--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,64 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.Nullable;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String NAME_TAG = "name";
+    private static final String MAIN_NAME_TAG = "mainName";
+    private static final String ALSO_KNOWN_AS_TAG = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN_TAG = "placeOfOrigin";
+    private static final String DESCRIPTION_TAG = "description";
+    private static final String IMAGE_TAG = "image";
+    private static final String INGREDIENTS_TAG = "ingredients";
+
+    @Nullable
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONObject nameObject = jsonObject.getJSONObject(NAME_TAG);
+            String mainName = nameObject.getString(MAIN_NAME_TAG);
+            JSONArray alsoKnownAsArray = nameObject.getJSONArray(ALSO_KNOWN_AS_TAG);
+            String placeOfOrigin = jsonObject.getString(PLACE_OF_ORIGIN_TAG);
+            String description = jsonObject.getString(DESCRIPTION_TAG);
+            String image = jsonObject.getString(IMAGE_TAG);
+            JSONArray ingredientsArray = jsonObject.getJSONArray(INGREDIENTS_TAG);
+
+            return new Sandwich(
+                    mainName,
+                    getListOfStringsFromJsonArray(alsoKnownAsArray),
+                    placeOfOrigin,
+                    description,
+                    image,
+                    getListOfStringsFromJsonArray(ingredientsArray));
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    private static List<String> getListOfStringsFromJsonArray(JSONArray jsonArray) {
+        try {
+            List<String> returnedList = new ArrayList<>(jsonArray.length());
+            for (int i = 0; i < jsonArray.length(); i++) {
+                returnedList.add(jsonArray.getString(i));
+            }
+
+            return returnedList;
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
